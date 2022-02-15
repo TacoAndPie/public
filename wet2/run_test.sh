91906793a68baf86e5ceafaf2a46ff3982062614
TacoAndPie <roymoshe23@gmail.com>
2022-02-15 23:13:10 +0200

    no message

--- wet2/run_test.sh
+++ wet2/run_test.sh
@@ -1,16 +1,16 @@
 rm a.out;
-for i in {0..0};
+for i in {0..14};
 do rm ../tests_out/my_out$i.txt;
 done
 
 g++ -std=c++11 -DNDEBUG -Wall *.cpp
 echo compiled
 
-for i in {0..0};
+for i in {0..14};
 do ./a.out < ../tests/in$i.txt > ../tests_out/my_out$i.txt;
 done
 
 
-for i in {0..0};
+for i in {0..14};
 do diff -s ../tests/out$i.txt  ../tests_out/my_out$i.txt;
 done
